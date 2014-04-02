ff5f16bedfd08ea8d75f9004f56559599afb5c6f
Yani0029 <yani@Yanis-MacBook-Air.local>
2014-04-02 13:23:03 +0200

    Rebuild the sendmail form and preview of mail, posible to send email with attachment

---
+++ js/valghalla_mail.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+  $('.js-show-volunteer-mail').click(function(event){
+    var id = $(this).attr('id');
+    var message = $('.js-volunteer-mail-text').html();
+    jQuery.get("/valhalla_mail/ajax/view/"+id, function(data){
+      $('.js-volunteer-mail').html(data);
+    });
+  });
+});
