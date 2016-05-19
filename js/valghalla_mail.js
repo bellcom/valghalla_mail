571190612fab0fe7ef99b8ad204621478ca1415f
Yani0029 <yani@bellcom.dk>
2016-05-19 14:57:30 +0200

    Updated module to version 2.0

--- js/valghalla_mail.js
+++ js/valghalla_mail.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
   $('.js-show-volunteer-mail').click(function(event){
     var id = $(this).attr('id');
     var message = $('.js-volunteer-mail-text').html();
-    jQuery.get("/valhalla_mail/ajax/view/"+id, function(data){
+    jQuery.get("/valghalla_mail/ajax/view/"+id, function(data){
       $('.js-volunteer-mail').html(data);
     });
   });
